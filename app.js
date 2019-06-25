bc3322fc39a8962f2248e7c7c68bc80bf2bc1d15
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-06-25 17:52:55 -0600

    added getElementById from zero - nine Btn

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var oneBtn = document.getElementById('calc-one');
+var twoBtn = document.getElementById('calc-two');
+var threeBtn = document.getElementById('calc-three');
+var fourBtn = document.getElementById('calc-four');
+var fiveBtn = document.getElementById('calc-five');
+var sixBtn = document.getElementById('calc-six');
+var sevenBtn = document.getElementById('calc-seven');
+var eightBtn = document.getElementById('calc-eight');
+var nineBtn = document.getElementById('calc-nine');
+var zeroBtn = document.getElementById('calc-zero');
